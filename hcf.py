e9944039f70ae1bdfab5feed06fd1d2c058a19bc
sana725z <anwarsana704@gmail.com>
2026-09-09 13:30:50 +0530

    hcf

---
+++ hcf.py
@@ -0,0 +1,10 @@
+def hcf(x,y):
+    if x<y:
+        smaller = x
+    else:
+        smaller=y
+    for i in range(1,smaller+1):
+        if x%i==0 and y%i==0:
+            hcf=i
+    return hcf
+print(hcf(3,6))
